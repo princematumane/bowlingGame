378599a484bcebf39eec52c641921fc4652be550
princematumane <princematumane@gmail.com>
2022-03-14 22:59:13 +0200

    add lazy loading routing components

--- UI/bowling-game/src/App.tsx
+++ UI/bowling-game/src/App.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 import { BrowserRouter, Route, Link, NavLink, Switch } from "react-router-dom";
-import Home from "./components/Pages/Home";
 import { hot } from 'react-hot-loader/root'
-import LeaderBoard from "./components/Pages/LeaderBoard";
-import Game from "./components/Pages/Game";
+import { Suspense } from "react";
 
 interface Props { }
 interface State {}
@@ -14,21 +12,29 @@ class App extends React.Component<Props, State> {
     constructor(props: any) {
         super(props)
     }
+    componentDidMount() {
+        this.setState({isLoading: false})
+    }
+
+ home = React.lazy(() => import('./components/Pages/Home'))
+ game = React.lazy(() => import('./components/Pages/Game'))
+ leaderBoard = React.lazy(() => import('./components/Pages/LeaderBoard'))
 
     render() {
-
         return (
             <div>
                     <BrowserRouter>
+                    <Suspense fallback={<div className="loader"></div>}>
                         <Switch>
-                            <Route exact path='/' component={Home}>
+                            <Route exact path='/' component={this.home}>
                             </Route>
-                            <Route exact path='/game' component={Game} />
-                            <Route exact path='/LeaderBoard' component={LeaderBoard} />
+                            <Route exact path='/game' component={this.game} />
+                            <Route exact path='/LeaderBoard' component={this.leaderBoard} />
                             <Route>
                                 Error 404 Not found
                                 </Route>
                         </Switch>
+                        </Suspense>
                     </BrowserRouter>
             </div>
         )
